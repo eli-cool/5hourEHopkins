3ace4c6f0280af6a9a164388b7e4934b2696787f
Eli Hopkins <eh128257@student.wpsok.org>
2024-08-22 13:02:09 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,21 @@
+#Name: Eli Hopkins
+#Class 5th Hour
+#Assignment: HW2
+
+print("hello world")
+
+print("what is your name?")
+
+user = input()
+
+print("Hello", user)
+
+print("what is your first number?")
+
+num1 = input()
+
+print("enter second number")
+
+num2 = input()
+
+print(num1 + num2, "is your number")

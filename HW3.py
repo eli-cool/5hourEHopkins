8550e948d1b60e1d1a5b8467f1f7dd64b4ca3cb8
Eli Hopkins <eh128257@student.wpsok.org>
2024-08-28 12:58:58 -0500

    added HW3

---
+++ HW3.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+list = ["john pear", "john apple", "john bannana", "john strayberry", "john grape"]
+print(list[4])
+
+nums = [4, 3, 8, 2]
+nums.insert(2,6)
+nums.sort()
+print(nums)
+print(nums[0] + nums[1] + nums[2])

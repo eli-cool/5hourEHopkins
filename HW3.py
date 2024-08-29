a9ed1b106809ad62ecfa9da5336f836451cabebc
Eli Hopkins <eh128257@student.wpsok.org>
2024-08-29 13:02:01 -0500

    added HW3

--- HW3.py
+++ HW3.py
@@ -2,16 +2,10 @@ print("hello world")
 
 list = ["john pear", "john apple", "john bannana", "john strayberry", "john grape"]
 list.append("john batman")
-print(list[4])
+print(list[3])
 
 nums = [4, 3, 8, 2]
-nums.insert(2,6)
-nums.sort()
+nums.insert(1,6)
 print(nums)
+nums.sort()
 print(nums[0] + nums[1] + nums[2])
-
-nums2 = [4, 3, 8, 2]
-nums2.insert(2,6)
-nums2.sort()
-print(nums2)
-

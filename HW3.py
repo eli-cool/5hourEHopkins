3618b46702ce9a2e33069d0fe5b64c344c9af3f4
Eli Hopkins <eh128257@student.wpsok.org>
2024-09-04 13:19:32 -0500

    added playground

--- HW3.py
+++ HW3.py
@@ -8,7 +8,13 @@ list.append("john batman")
 print(list[3])
 
 nums = [4, 3, 8, 2]
+
 nums.insert(1,6)
+
 print(nums)
+
 nums.sort()
+
 print(nums[0] + nums[1] + nums[2])
+
+
---
+++ playground.py
@@ -0,0 +1,9 @@
+#Eli Hopkins
+#Class: 5th hour
+#Assignment: HW3
+
+list1 =  [1, 4, 2, 8, 5, 10]
+lissum = 0
+for i in list1[:4]:
+    lissum = lissum + i
+print (lissum)

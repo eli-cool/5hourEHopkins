afeef1759e20a56c455d06c412c866ca84d2a91a
Eli Hopkins <eh128257@student.wpsok.org>
2024-09-05 13:19:41 -0500

    added playground

--- playground.py
+++ playground.py
@@ -1,9 +1,13 @@
 #Eli Hopkins
 #Class: 5th hour
 #Assignment: playground
+from random import randint
 
-list1 =  [1, 4, 2, 8, 5, 10]
-lissum = 0
-for i in list1[:4]:
-    lissum = lissum + i
-print (lissum)
+ran = randint (1,98)
+ran2 = 99 - ran
+
+for i in ran:
+    print("hello")
+print ("world")
+for x in ran2:
+    print("hello")

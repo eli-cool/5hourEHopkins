f64a87d3a700fd3d844847c9b0dff72f4c22ac9b
Eli Hopkins <eh128257@student.wpsok.org>
2024-09-06 13:16:25 -0500

    added playground

--- playground.py
+++ playground.py
@@ -3,11 +3,27 @@
 #Assignment: playground
 from random import randint
 
-ran = randint (1,98)
-ran2 = 99 - ran
+ran = randint (1,48)
+ran2 = 49 - ran
 
-for i in ran:
-    print("hello")
-print ("world")
-for x in ran2:
-    print("hello")
+total = 0
+
+for i in range(ran):
+    total = total + 1
+    print("hello", total)
+
+total = total + 1
+print ("world", total)
+tottrue = total
+
+for x in range(ran2):
+    total = total + 1
+    print("hello", total)
+
+while True:
+    ans = int(input("what number is \"world\" at? :"))
+    if ans == tottrue:
+        print("you got it!")
+        break
+    else:
+        print("try again")

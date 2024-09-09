45fe568a7550b6e954efb76f386bb94727aa8dc4
Eli Hopkins <eh128257@student.wpsok.org>
2024-09-09 13:19:10 -0500

    added playground

--- playground.py
+++ playground.py
@@ -6,24 +6,47 @@ from random import randint
 ran = randint (1,48)
 ran2 = 49 - ran
 
+points = 0
+
+lives = 3
+
 total = 0
-
-for i in range(ran):
-    total = total + 1
-    print("hello", total)
-
-total = total + 1
-print ("world", total)
-tottrue = total
-
-for x in range(ran2):
-    total = total + 1
-    print("hello", total)
-
 while True:
+
+    for i in range(ran):
+        total = total + 1
+        print("hello", total)
+
+    total = total + 1
+    print ("world", total)
+    tottrue = total
+
+    for x in range(ran2):
+        total = total + 1
+        print("hello", total)
+
     ans = int(input("what number is \"world\" at? :"))
     if ans == tottrue:
         print("you got it!")
-        break
+        points = points + 1
+        print("your score is", points)
+        while True:
+            a = int(input("say 1 when you want to continue:"))
+            if a == 1:
+                total = 0
+                break
     else:
-        print("try again")
+        lives = lives - 1
+        print("wRooOoNg")
+        print("you have", lives, " lives")
+
+        while True:
+            a = int(input("say 1 when you want to continue:"))
+            if a == 1:
+                total = 0
+                break
+
+    if lives == 0:
+        print("you have", lives," lives. YOu lOOsE!")
+        print("your score is", points)
+        break

7e8a689f30b1b57b9b70486d27459e54dc6dc700
Eli Hopkins <eh128257@student.wpsok.org>
2024-10-01 13:09:14 -0500

    added HW6

--- HW6.py
+++ HW6.py
@@ -6,9 +6,10 @@
 #Objectives
 
 #1. Print Hello World!
+print("he110 w0r1d")
 
 #2. Create a variable named num and assign it a number.
-
+num = 8
 #3. Create a nested if statement follows this structure:
 
 #If num is divisible by 2
@@ -24,3 +25,16 @@
 #       print the result of num being divided by 3
 #   else
 #       print that neither is divisible by 2 or 3
+if num % 2 == 0:
+    if num % 3 == 0:
+       print (num // 2)
+       print  (num // 3)
+    else:
+        print ("that it is not divisible by 3")
+        print (num // 2)
+else:
+   if num % 3 == 0:
+       print ("num is not divisible by 2")
+       print (num // 3)
+   else:
+       print ("neither is divisible by 2 or 3")

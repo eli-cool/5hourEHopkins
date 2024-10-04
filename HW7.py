7f01af27152d2ea0dfcfaa4146e05ecbfcb1fee9
Eli Hopkins <eh128257@student.wpsok.org>
2024-10-04 13:14:32 -0500

    added HW7

--- HW7.py
+++ HW7.py
@@ -5,22 +5,23 @@
 #1. Print Hello World!
 print("GREETINGS EARTHLINGS AHAHAHA")
 
-#2. Create three different boolean variables named wifi, login, and admin.
+#2. Create three different boolean variables named Wi-Fi, login, and admin.
 wifi = True
-login = False
-admin = False
+login = True
+admin = True
 #3. Create a separate integer variable that says the number of times
 #someone with admin credentials has logged in.
-amen = 0
-#4. Create a nested if statement that checks to see if wifi is true,
+amen = 2
+#4. Create a nested if statement that checks to see if Wi-Fi is true,
 #login is true, and admin is true. If they are all true, print a
 #welcome message and increase the integer variable by one. If one of them
 #is false, print an error message telling them which one they are "missing".
-if wifi == True:
-    if login == True:
-        if admin == True:
+if wifi:
+    if login:
+        if admin:
             print("welcome")
             amen += 1
+            print(amen, "admins have logged in")
         else:
             print("error you are missing the admin status")
     else:

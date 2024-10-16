11089f4b297af9a51ba5e9f41b1b72e224add6fd
Eli Hopkins <eh128257@student.wpsok.org>
2024-10-16 11:52:13 -0500

    added Playground

--- playground.py
+++ playground.py
@@ -3,50 +3,51 @@
 #Assignment: playground
 
 from random import randint
-
-ran = randint (1,48)
-ran2 = 49 - ran
-
+#separates 100 into 2 halfs
+half = randint (1,48)
+half2 = 49 - half
+#sets up the points
 points = 0
-
+#lives duh
 lives = 3
-
+#a number that keeps track where the World is in the Hellos
 total = 0
 while True:
-
-    for i in range(ran):
-        total = total + 1
+#for var half times it prints hello and keeps track what number it currently is on
+    for i in range(half):
+        total += 1
         print("hello", total)
-
+#prints world and tottrue takes the number it is on
     total = total + 1
     print ("world", total)
     tottrue = total
-
-    for x in range(ran2):
+#finishes the hello chain until its 100
+    for x in range(half2):
         total = total + 1
         print("hello", total)
-
+#ask question and checks whether it is tottrue which is the same number world is on
     ans = int(input("what number is \"world\" at? :"))
     if ans == tottrue:
         print("you got it!")
         points = points + 1
         print("your score is", points)
         while True:
+            #takes a break and asks you when you want to continue
             a = int(input("say 1 when you want to continue:"))
             if a == 1:
                 total = 0
                 break
+    # if you get the question wrong your lives go down
     else:
         lives = lives - 1
         print("wRooOoNg")
         print("you have", lives, " lives")
-
         while True:
             a = int(input("say 1 when you want to continue:"))
             if a == 1:
                 total = 0
                 break
-
+    #death check
     if lives == 0:
         print("you have", lives," lives. YOu lOOsE!")
         print("your score is", points)

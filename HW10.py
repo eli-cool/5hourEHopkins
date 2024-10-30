728c0d4c65948cec576b5fdf0a978c76f4794ec2
Eli Hopkins <eh128257@student.wpsok.org>
2024-10-30 13:03:10 -0500

    added HW8

--- HW10.py
+++ HW10.py
@@ -6,21 +6,23 @@ import time
 #1. Create a while loop with variable i that counts down from 5 to 0
 #and then prints "Hello World!" at the end.
 num = 5
-for i in range(num):
+while num >= 0:
     time.sleep(0.4)
-    print (num)
+    print(num)
     num -= 1
 #2. Create a while loop that prints only even numbers
 #between 1 and 30.
-for i in range(31):
-    if i % 2 == False:
-        time.sleep(0.4)
-        print(i)
+num2 = 32
+while num2 >= 0:
+    time.sleep(0.1)
+    print(num2)
+    num2 -= 2
 #3. Create a while loop that repeats until the user
 #inputs the number 0.
 while True:
     for i in range(10):
         print("bazinga")
-    ask = input("say 0 if you want to quit")
+    print("say 0")
+    ask = input("if you want to quit")
     if ask == "0":
         break

702b82bc3a717503c45142e7390b09c85b32c55a
Eli Hopkins <eh128257@student.wpsok.org>
2025-01-15 12:41:49 -0600

    added HW15.py

--- HW15.py
+++ HW15.py
@@ -4,7 +4,7 @@
 
 #1. Create a def function that prints out "Hello World!"
 def helo(num):
-    for i in range(num):
+    for i in range(int(num)):
         print("hello world")
 helo(3)
 #2. Create a def function that calculates the average of three numbers.
@@ -15,13 +15,15 @@ def calculate(a,b,c):
 #3. Create a def function with the names of 5 animals as arguments, treats it like a list, and
 #prints the name of the third animal.
 def anms(a1,a2,a3,a4,a5):
+    print(a3)
     return [a1,a2,a3,a4,a5]
 
 #4. Create a def function that loops from 1 to the number put in the argument.
 def loops(num):
-    for i in range(1,num,+1):
+    for i in range(1,num+1):
         print("jello wolrd")
 #5. Call all of the functions created in 1 - 4 with relevant arguments.
+helo(10)
 print (calculate(5,5,5))
 anms("cow","sheep","donkey","chicken","wendigo")
-print(anms("dog","cat","bird","kid","fish")[4])
+loops(4)

a709394713e922d03af1ea990c0ac6cd82462843
Eli Hopkins <eh128257@student.wpsok.org>
2025-01-31 12:46:41 -0600

    added HW19.py

---
+++ HW19.py
@@ -0,0 +1,12 @@
+#Name: Eli Hopkins
+#Class: 5th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+from HW15 import helo, anms, loops, calculate
+#2. Call the functions here and run HW19.
+#helo(10)
+#print (calculate(5,6,7))
+#anms("cow","sheep","donkeysese","chicken","wendigo")
+#loops(5)
+#3. Delete all calls from HW15 and run HW19 again.

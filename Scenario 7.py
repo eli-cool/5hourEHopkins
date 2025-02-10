fcf6150d22cdd7b800d56cfcc4db72fdcef5df5c
Eli Hopkins <eh128257@student.wpsok.org>
2025-02-10 13:19:03 -0600

    added scenario 7.py

---
+++ Scenario 7.py
@@ -0,0 +1,16 @@
+#Name: Eli Hopkins
+#Class: 5th Hour
+#Assignment: Scenario 7
+
+#Import all of Scenario 6 here
+from Scenario6 import *
+listAverage = 0
+
+def final_average():
+    global listAverage
+    listAverage = sum(statblock) / len(statblock)#Calculate the sum of the list by the length of the list here
+    return listAverage
+
+final_average()
+
+print(listAverage)
